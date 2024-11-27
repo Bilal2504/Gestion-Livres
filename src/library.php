aeecbc4ac4bf2d1168fc357453904a2007e68456
asil-x <x.zkx.98@gmail.com>
2024-11-27 10:30:11 +0100

    push des ficher

--- src/library.php
+++ src/library.php
@@ -4,10 +4,8 @@ namespace src;
 
 class Library
 {
-    // Utilisation d'un tableau simple au lieu d'une liste chaînée
     private $books = [];
 
-    // Création de livre (1 point)
     public function addBook($name, $description, $inStock = true)
     {
         $book = new Book($name, $description, $inStock);
@@ -15,10 +13,8 @@ class Library
         return $book;
     }
 
-    // Modification d'un livre (1 point)
     public function updateBook($id, $name, $description, $inStock)
     {
-        // Parcours simple du tableau - pourrait être optimisé
         foreach ($this->books as $key => $book) {
             if ($book->id === $id) {
                 $this->books[$key]->name = $name;
@@ -30,26 +26,22 @@ class Library
         return false;
     }
 
-    // Suppression d'un livre (1 point)
     public function deleteBook($id)
     {
         foreach ($this->books as $key => $book) {
             if ($book->id === $id) {
                 unset($this->books[$key]);
-                // On oublie de réindexer le tableau - petit défaut intentionnel
                 return true;
             }
         }
         return false;
     }
 
-    // Liste des livres (1 point)
     public function getAllBooks()
     {
         return $this->books;
     }
 
-    // Affichage d'un livre (1 point)
     public function getBook($id)
     {
         foreach ($this->books as $book) {
@@ -60,10 +52,8 @@ class Library
         return null;
     }
 
-    // Tri des livres (3 points au lieu de 4 - utilisation de tri à bulles au lieu de merge sort)
     public function sortBooks($column = 'name', $ascending = true)
     {
-        // Tri à bulles basique - moins efficace que merge sort
         $n = count($this->books);
         for ($i = 0; $i < $n - 1; $i++) {
             for ($j = 0; $j < $n - $i - 1; $j++) {
@@ -77,8 +67,6 @@ class Library
             }
         }
     }
-
-    // Fonction de comparaison pour le tri
     private function compareBooks($a, $b, $column)
     {
         switch ($column) {
@@ -92,11 +80,8 @@ class Library
                 return 0;
         }
     }
-
-    // Recherche d'un livre (4 points au lieu de 5 - recherche linéaire au lieu de binaire)
     public function searchBook($column, $value)
     {
-        // Recherche linéaire simple - moins efficace que recherche binaire
         foreach ($this->books as $book) {
             switch ($column) {
                 case 'name':
